0e3e4d9162b91a4ab113adaaec48c03653760272
Ray <reyna.acosta.olivas@gmial.com>
2023-08-10 22:06:01 -0600

    completed

---
+++ src/components/header/header.js
@@ -0,0 +1,9 @@
+import './header.css'
+
+function Header(){
+    return  <header className='header'>
+                <img src="/img/header.png" className="Org" alt="Org" />
+            </header>
+}
+
+export default Header
